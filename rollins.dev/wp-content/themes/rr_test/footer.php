11e871de959c8a684d8a3b11d58fd1a2e753833a
Mox Biggs <moxdev1@gmail.com>
2016-06-28 13:23:22 -0400

    added theme

---
+++ rollins.dev/wp-content/themes/rr_test/footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Rollins_Ridge
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'rr_test' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'rr_test' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'rr_test' ), 'rr_test', '<a href="http://www.mm4solutions.com/" rel="designer">Millennium Marketing Solutions</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
